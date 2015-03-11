b8a1671cf9fe19e719def4433f4eb0a72fd390b4
Ryan <ryanpauley@gmail.com>
2015-03-12 08:24:50 +1100

    fixed Jade rendering error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.set('views', __dirname, '/server/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
 app.get('*', function(req, res) {
@@ -12,5 +12,6 @@ app.get('*', function(req, res) {
 })
 
 var port = 3030;
+
 app.listen(port);
 console.log('listening on port: ' + port + "...");
